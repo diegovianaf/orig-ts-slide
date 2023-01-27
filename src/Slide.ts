ecd09ad7b2e401e11d69f64a0ab76addf8d22108
Diego Viana <diegovianaf7@gmail.com>
2023-01-27 11:44:25 -0300

    add pause function on click/hold slide

--- src/Slide.ts
+++ src/Slide.ts
@@ -8,6 +8,8 @@ export default class Slide {
   index: number
   slide: Element
   timeout: Timeout | null
+  pausedTimeout: Timeout | null
+  paused: boolean
 
   constructor(
     container: Element,
@@ -21,8 +23,10 @@ export default class Slide {
     this.time = time
 
     this.timeout = null
+    this.pausedTimeout = null
     this.index = 0
     this.slide = this.slides[this.index]
+    this.paused = false
 
     this.init()
   }
@@ -44,14 +48,32 @@ export default class Slide {
     this.timeout = new Timeout(() => this.next(), time)
   }
 
-  prev(){
+  prev() {
+    if (this.paused) return
     const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1
     this.show(prev)
   }
 
-  next(){
+  next() {
+    if (this.paused) return
     const next = this.index + 1 < this.slides.length ? this.index + 1 : 0
     this.show(next)
+  }
+
+  pause() {
+    console.log('pause')
+    this.pausedTimeout = new Timeout(() => {
+      this.paused = true
+    }, 300)
+  }
+
+  continue() {
+    console.log('continue')
+    this.pausedTimeout?.clear()
+    if (this.paused) {
+      this.paused = false
+      this.auto(this.time)
+    }
   }
 
   private addControls() {
@@ -64,6 +86,8 @@ export default class Slide {
     this.controls.appendChild(prevButton)
     this.controls.appendChild(nextButton)
 
+    this.controls.addEventListener('pointerdown', () => this.pause())
+    this.controls.addEventListener('pointerup', () => this.continue())
     prevButton.addEventListener('pointerup', () => this.prev())
     nextButton.addEventListener('pointerup', () => this.next())
   }
